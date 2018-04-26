7cdf77d9df2d000fb550943afa78a732e3cb26eb
Jan Mahdal <Jan.Mahdal365@seznam.cz>
2018-04-26 14:16:06 +0200

    init commit

---
+++ Asteroidy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 26 13:26:13 2018
+
+@author: mah35070
+"""
+
+import pyglet
+
+window = pyglet.window.Window()
+pyglet.app.run()
+print('Hotovo')
